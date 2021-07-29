73786fd2fef08dd3c8d7c4ea0855116aabad9cd3
Damaximum <79727388+Damaximum@users.noreply.github.com>
2021-07-28 18:14:43 -0700

    added db.js and Readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # CBC-Online-Offline-Budget-Tracker
+
+![Alt text](https://img.shields.io/badge/License-MIT-brightgreen "MIT")
+
+## Description
+
+​
+This project uses PWA to create a budget tracker.
+This tracker allows the user to create a budget then add or subtract from it.
+They can also use this app offline, and any changes will be saved locally then merged with the online version once back online.
+​
+
+## Table of Contents
+
+- [Description](#description)
+- [Table of Contents](#table-of-contents)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Usage
+
+​
+In order to use this application, visit the [deployed application]()
+​
+
+## License
+
+​
+This project is licensed under the MIT license.
+​
+
+## Questions
+
+​
+If you have any questions about the repo, open an issue or contact [Damaximum](https://github.com/Damaximum)
---
+++ db.js
@@ -0,0 +1,55 @@
+let db;
+const request = indexedDB.open("budgetTracker", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore("offlineBudget", {
+      autoIncrement: true,
+    });
+  }
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["offlineBudget"], "readwrite");
+  const store = transaction.objectStore("offlineBudget");
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["offlineBudget"], "readwrite");
+  const store = transaction.objectStore("offlineBudget");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["offlineBudget"], "readwrite");
+          const store = transaction.objectStore("offlineBudget");
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
